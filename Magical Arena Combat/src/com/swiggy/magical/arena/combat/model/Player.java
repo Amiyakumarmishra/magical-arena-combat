f07375b89f5458f5fb4af8adf5497e313c23641a
Amiyakumarmishra <amiyamishra122001@gmail.com>
2024-05-24 21:55:06 +0530

    code refactored

--- Magical Arena Combat/src/com/swiggy/magical/arena/combat/model/Player.java
+++ Magical Arena Combat/src/com/swiggy/magical/arena/combat/model/Player.java
@@ -4,38 +4,38 @@ import java.util.Random;
 
 public class Player {
 	private int health;
-    private int strength;
-    private int attack;
-    private Random random;
+	private int strength;
+	private int attack;
+	private Random random;
 
-    public Player(int health, int strength, int attack) {
-        this.health = health;
-        this.strength = strength;
-        this.attack = attack;
-        this.random = new Random();
-    }
+	public Player(int health, int strength, int attack) {
+		this.health = health;
+		this.strength = strength;
+		this.attack = attack;
+		this.random = new Random();
+	}
 
-    public int rollDice() {
-        return random.nextInt(6) + 1;
-    }
+	public int rollDice() {
+		return random.nextInt(6) + 1;
+	}
 
-    public int attack() {
-        return rollDice() * attack;
-    }
+	public int attack() {
+		return rollDice() * attack;
+	}
 
-    public int defend() {
-        return rollDice() * strength;
-    }
+	public int defend() {
+		return rollDice() * strength;
+	}
 
-    public void takeDamage(int damage) {
-        health = Math.max(0, health - damage);
-    }
+	public void takeDamage(int damage) {
+		health = Math.max(0, health - damage);
+	}
 
-    public int getHealth() {
-        return health;
-    }
+	public int getHealth() {
+		return health;
+	}
 
-    public boolean isAlive() {
-        return health > 0;
-    }
+	public boolean isAlive() {
+		return health > 0;
+	}
 }
