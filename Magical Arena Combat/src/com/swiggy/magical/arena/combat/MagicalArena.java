da021eb5abf68463db2044b9987639eb84485c3f
Amiyakumarmishra <amiyamishra122001@gmail.com>
2024-05-25 09:05:31 +0530

    Game logic Implemented

--- Magical Arena Combat/src/com/swiggy/magical/arena/combat/MagicalArena.java
+++ Magical Arena Combat/src/com/swiggy/magical/arena/combat/MagicalArena.java
@@ -1,10 +1,50 @@
 package com.swiggy.magical.arena.combat;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import com.swiggy.magical.arena.combat.model.Player;
+
 public class MagicalArena {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void playGame(Player player1, Player player2) {
+        Deque<Player> players = new ArrayDeque<>();
+        players.add(player1);
+        players.add(player2);
+        
+        boolean firstTurn = true;
+        while (player1.isAlive() && player2.isAlive()) {
+            Player attacker;
+            Player defender;
 
+            if (firstTurn) {
+                attacker = (player1.getHealth() < player2.getHealth()) ? player1 : player2;
+                defender = (attacker == player1) ? player2 : player1;
+                firstTurn = false;
+            } else {
+                attacker = players.pollFirst();
+                defender = players.peekFirst();
+                players.addLast(attacker);
+            }
+
+            System.out.println("Player " + attacker + " attacks");
+            int attackDamage = attacker.attack();
+            int defendDamage = defender.defend();
+            int damageTaken = Math.max(0, attackDamage - defendDamage);
+            defender.takeDamage(damageTaken);
+            System.out.println("Attack damage: " + attackDamage);
+            System.out.println("Defend damage: " + defendDamage);
+            System.out.println("Damage taken by " + defender + ": " + damageTaken);
+            System.out.println(defender + "'s health: " + defender.getHealth());
+            System.out.println();
+        }
+    }
+
+
+	public static void main(String[] args) {
+		Player playerA = new Player("A",500, 5, 10);
+		Player playerB = new Player("B",100, 10, 5);
+		playGame(playerA, playerB);
 	}
 
 }
--- Magical Arena Combat/src/com/swiggy/magical/arena/combat/model/Player.java
+++ Magical Arena Combat/src/com/swiggy/magical/arena/combat/model/Player.java
@@ -3,12 +3,15 @@ package com.swiggy.magical.arena.combat.model;
 import java.util.Random;
 
 public class Player {
+	
+	private String playerName;
 	private int health;
 	private int strength;
 	private int attack;
 	private Random random;
 
-	public Player(int health, int strength, int attack) {
+	public Player(String playerName,int health, int strength, int attack) {
+		this.playerName=playerName;
 		this.health = health;
 		this.strength = strength;
 		this.attack = attack;
@@ -38,4 +41,47 @@ public class Player {
 	public boolean isAlive() {
 		return health > 0;
 	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+
+	public Random getRandom() {
+		return random;
+	}
+
+	public void setRandom(Random random) {
+		this.random = random;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [playerName=" + playerName + ", health=" + health + "]";
+	}
+	
+	
 }
